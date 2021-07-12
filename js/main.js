d2e4c89fd53b0a1c85d7208f5a1c9d1d40727312
1-WebPages-1 <cabraljorge683@gmail.com>
2021-07-11 20:22:19 -0400

    Primer commit web Jc

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/* Menu */
+document.querySelector('.M-Icon-Container').addEventListener('click', () => 
+  {
+    document.querySelector('.nav-container').classList.toggle('show')
+  });
+
+
+/*
+
+ Cookies
+function compruebaAceptaCookies() {
+    if(localStorage.aceptaCookies == 'true'){
+      cajacookies.style.display = 'none';
+    }
+  }
+  
+  function aceptarCookies() {
+    localStorage.aceptaCookies = 'true';
+    cajacookies.style.display = 'none';
+  }
+  
+  $(document).ready(function () {
+    compruebaAceptaCookies();
+});
+   */
