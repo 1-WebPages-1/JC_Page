a97e703311ac4ff386a16a0069c897c3a9ac7842
1-WebPages-1 <cabraljorge683@gmail.com>
2021-07-20 02:25:32 -0400

    agregamos cookies y el pixel de facebook exitosamentegit add .

--- js/main.js
+++ js/main.js
@@ -4,21 +4,32 @@ document.querySelector('.M-Icon-Container').addEventListener('click', () =>
     document.querySelector('.nav-container').classList.toggle('show')
 });
 
- /* Cookies */
-// function compruebaAceptaCookies() {
-//     if(localStorage.aceptaCookies == 'true'){
-//       cajacookies.style.display = 'none';
-//     }
-//   }
-  
-//   function aceptarCookies() {
-//     localStorage.aceptaCookies = 'true';
-//     cajacookies.style.display = 'none';
-//   }
-  
-//   $(document).ready(function () {
-//     compruebaAceptaCookies();
-// });
+/* Cookies */
+const botonAceptarCookies = document.getElementById('btn-aceptar-cookies');
+const avisoCookies = document.getElementById('aviso-cookies');
+const fondoAvisoCookies = document.getElementById('fondo-aviso-cookies');
+
+dataLayer = [];
+
+if (!localStorage.getItem('cookies-aceptadas'))
+{
+  avisoCookies.classList.add('activo');
+  fondoAvisoCookies.classList.add('activo');
+} else {
+  dataLayer.push({'event': 'cookies-aceptadas'})
+}
+
+botonAceptarCookies.addEventListener('click', () => {
+  avisoCookies.classList.remove('activo');
+  fondoAvisoCookies.classList.remove('activo');
+
+  localStorage.setItem('cookies-aceptadas', true);
+
+  dataLayer.push({'event': 'cookies-aceptadas'});
+});
+
+
+
   
 
 /* sliders */
