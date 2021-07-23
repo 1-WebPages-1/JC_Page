cad23be81c5b0816f10bbc6746ad0ad85a05199d
1-WebPages-1 <cabraljorge683@gmail.com>
2021-07-22 20:09:49 -0400

    Agregando boton de facebook a la pagina

--- js/main.js
+++ js/main.js
@@ -31,6 +31,25 @@ botonAceptarCookies.addEventListener('click', () => {
 });
 
 
+/* Btn de facebook */
+var chatbox = document.getElementById('fb-customer-chat');
+chatbox.setAttribute("page_id", "101922521938025");
+chatbox.setAttribute("attribution", "biz_inbox");
 
-  
+window.fbAsyncInit = function() {
+  FB.init({
+    xfbml            : true,
+    version          : 'v11.0'
+  });
+};
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/es_ES/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
 
